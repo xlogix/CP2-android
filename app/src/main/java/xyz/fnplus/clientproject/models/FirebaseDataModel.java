64284d44b3e05b0c53fd9efdca5039ee03336f78
vijay krishna <krsnvijay@gmail.com>
2017-06-25 14:49:48 +0530

    Link With Database and Field Validation

--- app/src/main/java/xyz/fnplus/clientproject/models/FirebaseDataModel.java
+++ app/src/main/java/xyz/fnplus/clientproject/models/FirebaseDataModel.java
@@ -8,6 +8,7 @@ public class FirebaseDataModel {
     private String loomNumber;
     private String lastReading;
     private String date;
+    private String quality;
     private String shift;
     private String messSize;
     private String status;
@@ -22,7 +23,7 @@ public class FirebaseDataModel {
     }
 
     public FirebaseDataModel(String loomNumber, String lastReading, String date, String shift, String messSize, String status,
-                             String empCode, String empName, String dayOpenReading, String dayEndReading, String remark) {
+                             String empCode, String empName, String dayOpenReading, String dayEndReading, String remark, String quality) {
         this.loomNumber = loomNumber;
         this.lastReading = lastReading;
         this.date = date;
@@ -34,6 +35,7 @@ public class FirebaseDataModel {
         this.dayOpenReading = dayOpenReading;
         this.dayEndReading = dayEndReading;
         this.remark = remark;
+        this.quality = quality;
 
         // Initialize to current time
         time = new Date().getTime();
@@ -43,22 +45,97 @@ public class FirebaseDataModel {
         return date;
     }
 
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public String getDayEndReading() {
         return dayEndReading;
+    }
+
+    public void setDayEndReading(String dayEndReading) {
+        this.dayEndReading = dayEndReading;
     }
 
     public String getEmpCode() {
         return empCode;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setEmpCode(String empCode) {
+        this.empCode = empCode;
     }
 
     public String getMessSize() {
         return messSize;
     }
 
+    public void setMessSize(String messSize) {
+        this.messSize = messSize;
+    }
+
+    public String getLoomNumber() {
+        return loomNumber;
+    }
+
+    public void setLoomNumber(String loomNumber) {
+        this.loomNumber = loomNumber;
+    }
+
+    public String getLastReading() {
+        return lastReading;
+    }
+
+    public void setLastReading(String lastReading) {
+        this.lastReading = lastReading;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getDayOpenReading() {
+        return dayOpenReading;
+    }
+
+    public void setDayOpenReading(String dayOpenReading) {
+        this.dayOpenReading = dayOpenReading;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 
     public long getTime() {
         return time;
--- app/src/main/java/xyz/fnplus/clientproject/ui/MainActivity.java
+++ app/src/main/java/xyz/fnplus/clientproject/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package xyz.fnplus.clientproject.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -9,22 +10,34 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import xyz.fnplus.clientproject.R;
+import xyz.fnplus.clientproject.models.FirebaseDataModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -92,6 +105,20 @@ public class MainActivity extends AppCompatActivity {
     AppBarLayout mAppBar;
     @BindView(R.id.fab)
     FloatingActionButton mFab;
+    @BindView(R.id.shiftdate)
+    EditText mShiftdate;
+    @BindView(R.id.rb_day)
+    RadioButton mRbDay;
+    @BindView(R.id.rb_night)
+    RadioButton mRbNight;
+    @BindView(R.id.rg_shift_details)
+    RadioGroup mRgShiftDetails;
+    @BindView(R.id.form_layout)
+    LinearLayout mFormLayout;
+    @BindView(R.id.txt_last_reading_date)
+    TextView mTxtLastReadingDate;
+    @BindView(R.id.txt_last_reading_shift)
+    TextView mTxtLastReadingShift;
     private DatabaseReference mFirebaseDatabaseReference;
 
     @Override
@@ -101,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mFormLayout.setVisibility(View.GONE);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -145,13 +172,187 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.btn_loom_submit)
-    public void onMBtnLoomSubmitClicked() {
+    public void onMBtnLoomSubmitClicked(final View view) {
+        if (TextUtils.isEmpty(mEditTxtLoom.getText().toString())) {
+            mEditTxtLoom.setError("Enter Loom No");
+            Toast.makeText(this, "Enter Loom No", Toast.LENGTH_SHORT).show();
+        } else {
+            mFirebaseDatabaseReference.child("LOOMS").child(mEditTxtLoom.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    FirebaseDataModel dm = dataSnapshot.getValue(FirebaseDataModel.class);
+                    if (dm == null) {
+                        clearDatainView();
+                        mFormLayout.setVisibility(View.VISIBLE);
+                        Snackbar.make(view, "No Loom Found,Fill The Details For that Loom", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    } else {
+                        clearDatainView();
+                        Snackbar.make(view, "Last Record Of the Loom Fetched!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        mFormLayout.setVisibility(View.VISIBLE);
+                        setDataToView(dm);
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
 
 
     }
 
     @OnClick(R.id.btn_details_submit)
-    public void onMBtnDetailsSubmitClicked() {
+    public void onMBtnDetailsSubmitClicked(View v) {
+        if (validateDetails()) {
+            RadioButton shift = (RadioButton) findViewById(mRgShiftDetails.getCheckedRadioButtonId());
+            RadioButton messsize = (RadioButton) findViewById(mRgMess.getCheckedRadioButtonId());
+            RadioButton status = (RadioButton) findViewById(mRgStatus.getCheckedRadioButtonId());
+            FirebaseDataModel model = new FirebaseDataModel(mEditTxtLoom.getText().toString(), mTextViewDayOpenreading.getText().toString(), mShiftdate.getText().toString(), shift.getText().toString(), messsize.getText().toString(), status.getText().toString(), mTxtEmpCode.getText().toString(), mTxtEmpName.getText().toString(), mTextViewDayOpenreading.getText().toString(), mTextViewDayClosereading.getText().toString(), mEditTxtRemark.getText().toString(), mTextView.getText().toString());
+            mFirebaseDatabaseReference.child("LOOMS").child(mEditTxtLoom.getText().toString()).setValue(model);
+            Snackbar.make(v, "Loom Updated", Snackbar.LENGTH_LONG).show();
+            mFormLayout.setVisibility(View.GONE);
+        }
 
     }
+
+    boolean validateDetails() {
+        boolean validate = true;
+        if (TextUtils.isEmpty(mEditTxtLoom.getText().toString())) {
+            validate = false;
+            mEditTxtLoom.setError("Enter Loom No");
+            Toast.makeText(this, "Enter Loom No", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(mShiftdate.getText().toString())) {
+            validate = false;
+            mShiftdate.setError("Enter Shift Date");
+            Toast.makeText(this, "Enter Shift Date", Toast.LENGTH_SHORT).show();
+        }
+        if (mRgShiftDetails.getCheckedRadioButtonId() == -1) {
+            validate = false;
+            Toast.makeText(this, "Select Mess Size or Status", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(mTextView.getText().toString())) {
+            validate = false;
+            mTextView.setError("Enter Quality Name/Code");
+            Toast.makeText(this, "Enter Quality Name/Code", Toast.LENGTH_SHORT).show();
+        }
+        if (mRgMess.getCheckedRadioButtonId() == -1 || mRgStatus.getCheckedRadioButtonId() == -1) {
+            validate = false;
+            Toast.makeText(this, "Select Mess Size or Status", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(mTxtEmpCode.getText().toString()) || TextUtils.isEmpty(mTxtEmpName.getText().toString())) {
+            validate = false;
+            mTxtEmpCode.setError("Enter Employee Name/Code");
+            Toast.makeText(this, "Enter Employee Name/Code", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(mTextViewDayOpenreading.getText().toString()) || TextUtils.isEmpty(mTextViewDayClosereading.getText().toString())) {
+            validate = false;
+            mTextViewDayOpenreading.setError("Enter Day Close/Open Reading");
+            Toast.makeText(this, "Enter Day Close/Open Reading", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(mEditTxtRemark.getText().toString())) {
+            validate = false;
+            mEditTxtRemark.setError("Enter Remark");
+            Toast.makeText(this, "Enter Remark", Toast.LENGTH_SHORT).show();
+        }
+
+
+        return validate;
+
+    }
+
+    @OnClick(R.id.shiftdate)
+    public void onViewClicked() {
+
+        Calendar newCalendar = Calendar.getInstance();
+
+        DatePickerDialog StartTime = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy", Locale.UK);
+
+                mShiftdate.setText(sdf.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        StartTime.show();
+    }
+
+    void setDataToView(FirebaseDataModel model) {
+        mDbLastReading.setText(model.getLastReading());
+        mTxtLastReadingDate.setText(model.getDate());
+        mTxtLastReadingShift.setText(model.getShift());
+        mShiftdate.setText(model.getDate());
+        mTxtEmpCode.setText(model.getEmpCode());
+        mTxtEmpName.setText(model.getEmpName());
+        mTextViewDayClosereading.setText(model.getDayEndReading());
+        mTextViewDayOpenreading.setText(model.getDayOpenReading());
+        mEditTxtRemark.setText(model.getRemark());
+        mTextView.setText(model.getQuality());
+        switch (model.getShift()) {
+            case "Day":
+                mRbDay.setChecked(true);
+                break;
+            case "Night":
+                mRbNight.setChecked(true);
+                break;
+
+        }
+        switch (model.getMessSize()) {
+            case "10*10":
+                mRbMess1.setChecked(true);
+                break;
+            case "11*11":
+                mRbMess2.setChecked(true);
+                break;
+            case "12*12":
+                mRbMess3.setChecked(true);
+                break;
+            case "Other":
+                mRbMess4.setChecked(true);
+                break;
+        }
+        switch (model.getStatus()) {
+            case "Running":
+                mRbStatus1.setChecked(true);
+                break;
+            case "Drawing":
+                mRbStatus2.setChecked(true);
+                break;
+            case "Loading":
+                mRbStatus3.setChecked(true);
+                break;
+            case "R&M":
+                mRbStatus4.setChecked(true);
+                break;
+            case "Other":
+                mRbStatus5.setChecked(true);
+                break;
+        }
+
+    }
+
+    void clearDatainView() {
+        mDbLastReading.setText("N/A");
+        mTxtLastReadingDate.setText("N/A");
+        mTxtLastReadingShift.setText("N/A");
+        mShiftdate.setText(null);
+        mTxtEmpCode.setText(null);
+        mTxtEmpName.setText(null);
+        mTextViewDayClosereading.setText(null);
+        mTextViewDayOpenreading.setText(null);
+        mEditTxtRemark.setText(null);
+        mTextView.setText(null);
+        mRgStatus.clearCheck();
+        mRgMess.clearCheck();
+        mRgShiftDetails.clearCheck();
+    }
 }
+
